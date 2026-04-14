d394546ec8242f9b0da5314a1d817fc0aa37e8f9
Dinesh Tomar <dinesh.tomar@dapt.tech>
2026-04-14 18:16:22 +0530

    refine: updated vite config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,18 +2,23 @@ import path from 'node:path'
 
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
+    base: env.VITE_BASE_URL || '/',
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './src'),
+      },
     },
-  },
-  server: {
-    host: '127.0.0.1',
-    port: 5173,
-  },
+    server: {
+      host: '127.0.0.1',
+      port: 5173,
+    },
+  }
 })
